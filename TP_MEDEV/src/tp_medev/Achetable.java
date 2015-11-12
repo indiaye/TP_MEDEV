2836d8a3d8320199d3c03c9d9097437b3f51d9bc
Hicham <hicham.daher@eleves.ec-nantes.fr>
2015-11-12 11:15:12 +0100

    Ajout demande achatjoueur

--- TP_MEDEV/src/tp_medev/Achetable.java
+++ TP_MEDEV/src/tp_medev/Achetable.java
@@ -122,4 +122,9 @@ public abstract class Achetable extends Case {
     }
     
     public abstract int calculLoyer(Joueur j);
+    
+    public void demandeAchatJoueur(Joueur j){
+        
+        System.out.println("");
+    }
 }
