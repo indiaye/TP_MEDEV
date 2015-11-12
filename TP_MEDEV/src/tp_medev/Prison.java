b0f5d268b3ab1b9cd7e3234c6dd9b2c962fb299a
SebastienGondron <sebastien.gondron@gmail.com>
2015-11-12 08:59:48 +0100

    Ajout de Prison et Depart

---
+++ TP_MEDEV/src/tp_medev/Prison.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp_medev;
+
+/**
+ *
+ * @author Seb
+ */
+public class Prison extends Case {
+
+    public Prison(String Nom, int Numero) {
+        super(Nom, Numero);
+    }
+    
+}
