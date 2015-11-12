3dcbad13fe582b6e3faf16b96ab517e9f2f7a27d
Rominet13 <volphoenix@outlook.fr>
2015-11-12 10:29:31 +0100

    Constructible reste juste à faire ConstructionMaison... Gare rajout de calculLoyer non fini, il faut gérer le nb de gare du propriétaire

--- TP_MEDEV/src/tp_medev/Constructible.java
+++ TP_MEDEV/src/tp_medev/Constructible.java
@@ -1,4 +1,3 @@
-
 package tp_medev;
 
 /**
@@ -7,77 +6,94 @@ package tp_medev;
  */
 public class Constructible extends Achetable {
 
+    private boolean[] nbMaisons;
     private int a;
     private int b;
-    private int coutMaison ;
+    private int coutMaison;
     private int coutHotel;
     private int[] loyers;
     private int posLoyer;
-    
-/**
- * Attention le dernier paramétre est de la première case du tableau loyers
- * @param loyerBase
- * @param a
- * @param b
- * @param coutMaison
- * @param coutHotel
- * @param prix
- * @param proprietaire
- * @param nom
- * @param numero 
- */
-    public Constructible(int loyerBase,int a, int b, int coutMaison, int coutHotel, int prix, Joueur proprietaire, String nom, int numero) {
+
+    /**
+     * Attention le dernier paramétre est de la première case du tableau loyers
+     *
+     * @param loyerBase
+     * @param a
+     * @param b
+     * @param coutMaison
+     * @param coutHotel
+     * @param prix
+     * @param proprietaire
+     * @param nom
+     * @param numero
+     */
+    public Constructible(int loyerBase, int a, int b, int coutMaison, int coutHotel, int prix, Joueur proprietaire, String nom, int numero) {
         super(prix, proprietaire, nom, numero);
-       this.a = a; //loyer avec une maison
+        for (int i = 0; i < 6; i++) {
+            nbMaisons[i] = false;
+        }
+        this.a = a; //loyer avec une maison
         this.b = b; //loyer avec un hotel (il faut 4 maison avant)
         this.coutMaison = coutMaison;
         this.coutHotel = coutHotel;
-        loyers[0]=loyerBase;
+        loyers[0] = loyerBase;
         remplirLoyer();
         posLoyer = 0;
     }
 
-    
     /**
      * utilisé uniquement dans le constructeur
      */
-    public void remplirLoyer(){
-        for(int i=1;i<5;i++){
-            loyers[i]=a*i;
+    public void remplirLoyer() {
+        for (int i = 1; i < 5; i++) {
+            loyers[i] = a * i;
         }
-        loyers[5]+=b;
+        loyers[5] += b;
     }
-    
-    public void DemandeConstruction(){
-        
+
+    public void DemandeConstruction() {
+
     }
+
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     @Override
     public String toString() {
 
-        
         String resultat = super.toString() + ", ";
         switch (posLoyer) {
             case 0:
                 resultat += "Pas de maison, ";
                 break;
             case 1:
-                resultat += "1 maison, " ;
-                break ;
+                resultat += "1 maison, ";
+                break;
             case 5:
                 resultat += "1 hotel, ";
                 break;
             default:
                 resultat += posLoyer + " maisons, ";
         }
-        
-        resultat += "loyer = " ;
+
+        resultat += "loyer = ";
         resultat += this.loyers[posLoyer];
-        
-        return resultat; 
+
+        return resultat;
     }
 
+    public int calculLoyer(Joueur joueur) {
+        int loyer = 0;
+        try {
+            if ((this.getProprietaire() != null) && (!joueur.getNom().equals(this.getProprietaire().getNom()))) {
+                loyer = this.loyers[posLoyer];
+            }
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return loyer;
+
+    }
 }
