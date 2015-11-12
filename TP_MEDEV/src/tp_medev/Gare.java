9e9d93a04df97260c649326f1c56ad8067844cd6
SebastienGondron <sebastien.gondron@gmail.com>
2015-11-12 08:58:41 +0100

    Ajout de gare

---
+++ TP_MEDEV/src/tp_medev/Gare.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp_medev;
+
+/**
+ *
+ * @author Seb
+ */
+public class Gare extends Achetable {
+
+    public Gare(int prix, Joueur proprietaire, String Nom, int Numero) {
+        super(prix, proprietaire, Nom, Numero);
+    }
+
+    
+}
