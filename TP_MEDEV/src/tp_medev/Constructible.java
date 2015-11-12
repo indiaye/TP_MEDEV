fc4b063e1ccb3ba4613ee9f5801b4719dbb74f12
Rominet13 <volphoenix@outlook.fr>
2015-11-12 09:56:35 +0100

    Ajout constructeur dans Constructible 1

--- TP_MEDEV/src/tp_medev/Constructible.java
+++ TP_MEDEV/src/tp_medev/Constructible.java
@@ -7,7 +7,48 @@ package tp_medev;
  */
 public class Constructible extends Achetable {
 
+     private int a;
+    private int b;
+    private int coutMaison ;
+    private int coutHotel;
+    private int[] loyers;
+    private int posLoyer;
+
+    /**
+     * 
+     * @param a
+     * @param b
+     * @param coutMaison
+     * @param coutHotel
+     * @param loyerBase 
+     * Attention le dernier paramétre est de la première case du tableau loyers
+     * 
+     * Attention SUPER A RAJOUTER !!!!!
+     */
+    public Constructible(int a, int b, int coutMaison, int coutHotel,int loyerBase) {
+        this.a = a; //loyer avec une maison
+        this.b = b; //loyer avec un hotel (il faut 4 maison avant)
+        this.coutMaison = coutMaison;
+        this.coutHotel = coutHotel;
+        loyers[0]=loyerBase;
+        remplirLoyer();
+        posLoyer = 0;
+    }
     
+   
+    /**
+     * utilisé uniquement dans le constructeur
+     */
+    public void remplirLoyer(){
+        for(int i=1;i<5;i++){
+            loyers[i]=a*i;
+        }
+        loyers[5]+=b;
+    }
+    
+    public void DemandeConstruction(){
+        
+    }
     /**
      * 
      * @return 
